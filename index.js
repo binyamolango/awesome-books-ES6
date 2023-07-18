c058d041df66ca9aeafa2ae80f73f1f955a92a84
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-07-18 10:03:30 +0300

    Fix linter error

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import bookManager from './Modules/manage-books.js';
+// eslint-disable-next-line
+import './Modules/manage-books.js';
 import './modules/page-change.js';
 import './modules/display-time.js';
-
-bookManager.update();
--- modules/page-change.js
+++ modules/page-change.js
@@ -5,54 +5,53 @@ const listLink = document.getElementById('list-link');
 const formLink = document.getElementById('form-link');
 const contactLink = document.getElementById('contact-link');
 
-
 listLink.addEventListener('click', () => {
-    if (!addNew.classList.contains('hidden')) {
-      addNew.classList.add('hidden');
-    }
-  
-    if (!contact.classList.contains('hidden')) {
-      contact.classList.add('hidden');
-    }
-  
-    if (list.classList.contains('hidden')) {
-      list.classList.remove('hidden');
-    }
-    listLink.style.color = 'blue';
-    formLink.style.color = 'black';
-    contactLink.style.color = 'black';
+  if (!addNew.classList.contains('hidden')) {
+    addNew.classList.add('hidden');
+  }
+
+  if (!contact.classList.contains('hidden')) {
+    contact.classList.add('hidden');
+  }
+
+  if (list.classList.contains('hidden')) {
+    list.classList.remove('hidden');
+  }
+  listLink.style.color = 'blue';
+  formLink.style.color = 'black';
+  contactLink.style.color = 'black';
 });
 
 formLink.addEventListener('click', () => {
-    if (!list.classList.contains('hidden')) {
-      list.classList.add('hidden');
-    }
-  
-    if (!contact.classList.contains('hidden')) {
-      contact.classList.add('hidden');
-    }
-  
-    if (addNew.classList.contains('hidden')) {
-      addNew.classList.remove('hidden');
-    }
-    listLink.style.color = 'black';
-    formLink.style.color = 'blue';
-    contactLink.style.color = 'black';
+  if (!list.classList.contains('hidden')) {
+    list.classList.add('hidden');
+  }
+
+  if (!contact.classList.contains('hidden')) {
+    contact.classList.add('hidden');
+  }
+
+  if (addNew.classList.contains('hidden')) {
+    addNew.classList.remove('hidden');
+  }
+  listLink.style.color = 'black';
+  formLink.style.color = 'blue';
+  contactLink.style.color = 'black';
 });
 
 contactLink.addEventListener('click', () => {
-    if (!list.classList.contains('hidden')) {
-      list.classList.add('hidden');
-    }
-  
-    if (!addNew.classList.contains('hidden')) {
-      addNew.classList.add('hidden');
-    }
-  
-    if (contact.classList.contains('hidden')) {
-      contact.classList.remove('hidden');
-    }
-    listLink.style.color = 'black';
-    formLink.style.color = 'black';
-    contactLink.style.color = 'blue';
+  if (!list.classList.contains('hidden')) {
+    list.classList.add('hidden');
+  }
+
+  if (!addNew.classList.contains('hidden')) {
+    addNew.classList.add('hidden');
+  }
+
+  if (contact.classList.contains('hidden')) {
+    contact.classList.remove('hidden');
+  }
+  listLink.style.color = 'black';
+  formLink.style.color = 'black';
+  contactLink.style.color = 'blue';
 });
